9327607ec2c7abbfbfe11906bb261617c524b66b
Facundo Rodrigues <facuurodrigues@gmail.com>
2022-09-02 10:56:57 -0300

    -Se agrega solution.
    -Se agrega Modules, Shared y API

---
+++ RecommendationArt/Shared.Infrastructure/Controllers/InternalControllerFeatureProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Infrastructure.Controllers
+{
+    internal class InternalControllerFeatureProvider
+    {
+        protected override bool IsController(TypeInfo typeInfo)
+        {
+            if (!typeInfo.IsClass)
+            {
+                return false;
+            }
+            if (typeInfo.IsAbstract)
+            {
+                return false;
+            }
+            if (typeInfo.ContainsGenericParameters)
+            {
+                return false;
+            }
+            if (typeInfo.IsDefined(typeof(NonControllerAttribute)))
+            {
+                return false;
+            }
+            return typeInfo.Name.EndsWith("Controller", StringComparison.OrdinalIgnoreCase) ||
+                    typeInfo.IsDefined(typeof(ControllerAttribute));
+        }
+    }
+}
