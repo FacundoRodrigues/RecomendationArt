4b23a1c5f5fdf7acdbc1ebc1cc108d374db8b20d
Facundo Rodrigues <facuurodrigues@gmail.com>
2022-09-04 17:30:43 -0300

    Se hace refactor de Program
    Se agrega Startup.cs
    Se agrega RecommendationController

---
+++ RecommendationArt/API/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.OpenApi.Models;
+using Shared.Infrastructure.Extensions;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSharedInfrastructure(Configuration);
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        // Configure the HTTP request pipeline.
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+    }
+}
---
+++ RecommendationArt/Modules/Recommendation/Modules.Recommendation/Controllers/RecommendationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modules.Recommendation.Controllers
+{
+    [ApiController]
+    [Route("/api/catalog/[controller]")]
+    internal class RecommendationController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            return Ok();
+        }
+    }
+}
