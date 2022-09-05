8f6bc7a4d61f4f2ff6721f5e932fa7fa1d5647e1
Facundo Rodrigues <facuurodrigues@gmail.com>
2022-09-05 00:03:20 -0300

    Agrega Recommendation entity a Recommendation.Core.
    Agrega ModuleDbContext genérico
    Agrega interfaz IRecommendationDbContext
    Agrega RecommendationDbContext

---
+++ RecommendationArt/Modules/Recommendation/Modules.Recommendation.Core/Entities/Recommendation.cs
@@ -0,0 +1,14 @@
+﻿namespace Modules.Recommendation.Core.Entities
+{
+    public class Recommendation
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public double TrueRating { get; set; }
+        public string JokeRating { get; set; }
+        public string Description { get; set; } 
+        public string Platform { get; set; }
+        public string Occasion { get; set; }
+    }
+}
