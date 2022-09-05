72210772ae229be06dfb7326acb7f6a69463e52d
Facundo Rodrigues <facuurodrigues@gmail.com>
2022-09-04 22:23:14 -0300

    Se modifica Startup y Program.
    Swagger ahora detecta cualquier controller por fuera de la API

--- RecommendationArt/API/Startup.cs
+++ RecommendationArt/API/Startup.cs
@@ -10,15 +10,20 @@ public class Startup
 
     public IConfiguration Configuration { get; }
 
+    // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+        services.AddControllers();
+        //services.AddEndpointsApiExplorer();
+
         services.AddSharedInfrastructure(Configuration);
         services.AddSwaggerGen(c =>
         {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Recommendation API", Version = "v1" });
         });
     }
 
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
         // Configure the HTTP request pipeline.
@@ -31,5 +36,7 @@ public class Startup
         app.UseHttpsRedirection();
 
         app.UseAuthorization();
+
+        //app.UseSharedInfraestructure(env);
     }
 }
